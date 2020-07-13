ea01006b319feef29216fdc48d408bc46a55503a
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-14 02:41:44 +0530

    Jul 14  02:41:44

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -5,6 +5,14 @@
 # distance(x1, y1, x2, y2), which you might call several times. Also, remember to use
 # almostEqual (instead of ==) when comparing floats.
 
+import math
+
+
+def dist(x1, y1, x2, y2):
+    return math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+
+
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
-	# your code goes here
-	pass
+    # your code goes here
+
+    pass
