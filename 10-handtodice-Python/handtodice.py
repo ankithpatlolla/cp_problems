f765ddeb890be55e28620770607f20ed616fcf14
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-14 00:32:54 +0530

    Jul 14  00:32:54

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -6,5 +6,9 @@
 # Hint: You might find // and % useful here, and also getKthDigit().
 
 def handtodice(hand):
-	# your code goes here
-	pass
+    # your code goes here
+    l = []
+    while hand > 0:
+        l.append(hand % 10)
+        hand = hand // 10
+    return tuple(l[::-1])
