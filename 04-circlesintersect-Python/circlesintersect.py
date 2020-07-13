44bb1bb712b4579aa476eea0c3c079d636b51589
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-13 19:59:43 +0530

    Jul 13  19:59:43

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -8,7 +8,6 @@ def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
     # your code goes here
     points_sum = ((x1 - x2) ** 2) + ((y1 - y2) ** 2)
     radi_sum = (r1 + r2) ** 2
-    if points_sum < radi_sum:
+    if points_sum <= radi_sum:
         return True
-
     return False
