e37f36bebb5faa366693e17f6b509f3d88ea937f
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-14 21:52:27 +0530

    Jul 14  21:52:26

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -4,15 +4,18 @@ you'd like! Try to write each one in as
 few lines as possible.
 Make sure you pass the test cases too!"""
 
+
 class Queue:
     def __init__(self, head=None):
         self.storage = [head]
 
     def enqueue(self, new_element):
-        pass
+        self.storage.append(new_element)
 
     def peek(self):
-        pass 
+        return self.storage[0]
 
     def dequeue(self):
-        pass
+        temp = self.storage[len(storage[-1])]
+        self.storage = self.storage[:- 1]
+        return temp
