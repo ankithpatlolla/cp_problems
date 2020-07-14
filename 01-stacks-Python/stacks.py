2845f8cb1e12e1955d328a1f641e1559af797b40
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-14 21:31:22 +0530

    Jul 14  21:31:22

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -37,7 +37,8 @@ class LinkedList(object):
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
         temp = self.head
-        self.head = self.head.next
+        if self.head != None:
+            self.head = self.head.next
         return temp
 
 
