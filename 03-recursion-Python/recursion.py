8c4ec4e8a97a9f2bf721bb4698f8ef50dfca8230
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-14 22:32:59 +0530

    Jul 14  22:32:59

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -3,5 +3,10 @@ Fibonacci sequence value.
 Your code should have the same input/output as the 
 iterative code in the instructions."""
 
+
 def get_fib(position):
-    return -1
+    if position == 0:
+        return 0
+    elif position == 1:
+        return 1
+    return get_fib(position - 1) + get_fib(position - 2)
