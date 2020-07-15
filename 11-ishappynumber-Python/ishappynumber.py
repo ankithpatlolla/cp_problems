27b8b30f24598328c4cf086149f9fe625a705ead
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-16 01:47:19 +0530

    Jul 16  01:47:19

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -27,6 +27,8 @@ def ishappynumber(n):
     # your code goes here
     if n == 1 or n == 7:
         return True
+    elif n == 0 or n == 4:
+        return False
     else:
         if n < 10:
             return ishappynumber(n ** 2)
@@ -35,6 +37,4 @@ def ishappynumber(n):
                 check = sum_num(n)
                 if check < 10 and check != 1:
                     return False
-                elif check == 1 or check == 7:
-                    return True
                 return ishappynumber(check)
