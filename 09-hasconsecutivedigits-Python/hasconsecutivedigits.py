1b7c4a6c5347bc62f4c187d25b4e390773c9ff2b
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-16 00:22:15 +0530

    Jul 16  00:22:15

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -1,7 +1,14 @@
 # hasConsecutiveDigits(n)  [10pts]
-# Write the function hasConsecutiveDigits(n) that takes a possibly negative int value n and returns True if that 
+# Write the function hasConsecutiveDigits(n) that takes a possibly negative int value n and returns True if that
 # number contains two consecutive digits that are the same, and False otherwise.
 
 def hasconsecutivedigits(n):
-	# your code goes here
-	pass
+    # your code goes here
+    prev = 0
+    n = abs(n)
+    while n > 0:
+        if prev == n % 10:
+            return True
+        prev = n % 10
+        n = n // 10
+    return False
