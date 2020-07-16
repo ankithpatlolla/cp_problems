db9d3712a8cd3f710e04943c127408fdeed3c18f
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-16 21:09:39 +0530

    Jul 16  21:09:39

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -5,19 +5,21 @@
 
 
 def fun_replace(s1, s2, s3):
-    res = ""
-    for i in range(len(s1)):
-        check = s1[i]
-        j = i + 1
-        while j < len(s1):
-            check += s1[j]
-            if len(check) == len(s2):
-                break
-            j += 1
-        if check == s2:
-            res += s1[:i] + s3 + s1[j + 1:]
-            if (res.find(s2) == -1):
-                return res
-            else:
-                return fun_replace(res, s2, s3)
-    return s1
+    # res = ""
+    # for i in range(len(s1)):
+    #     check = s1[i]
+    #     j = i + 1
+    #     while j < len(s1):
+    #         check += s1[j]
+    #         if len(check) == len(s2):
+    #             break
+    #         j += 1
+    #     if check == s2:
+    #         res += s1[:i] + s3 + s1[j + 1:]
+    #         if (res.find(s2) == -1):
+    #             return res
+    #         else:
+    #             return fun_replace(res, s2, s3)
+    # return s1
+    arr = s1.split(s2)
+    return s3.join(arr)
