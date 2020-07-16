0a1aaad5629a2db981290bb9048fed10279aff07
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-16 19:17:45 +0530

    Jul 16  19:17:45

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -10,12 +10,9 @@ def fun_interleave(s1, s2):
     res = ""
     n = min(len(s1), len(s2))
     m = sorted([s1, s2], key=len)
-    if len(s1) == len(s2):
-        rest = ""
-    else:
-        rest = m[1][len(m[1]) - len(m[0]) - 2:]
+
     for i in range(n):
         res += s1[i] + s2[i]
         if i == n - 1:
-            res += rest
+            res += m[1][i:]
     return res
