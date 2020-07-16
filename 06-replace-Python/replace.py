cfcd29b973c27acde005ecabeae72db8ce59e074
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-16 19:36:44 +0530

    Jul 16  19:36:43

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -8,7 +8,7 @@ def fun_replace(s1, s2, s3):
     res = ""
     for i in range(len(s1)):
         check = s1[i]
-        for j in range(i, len(s1)):
+        for j in range(i + 1, len(s1)):
             check += s1[j]
             if check == s2:
                 res += s1[:i] + s1[i:j] + s1[j:]
