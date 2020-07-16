d8a3e4d4f52762ef2913885c668446f34eab41fe
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-16 14:54:53 +0530

    Jul 16  14:54:53

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -16,5 +16,5 @@ def fun_applycaesarcipher(msg, shift):
         if i == " ":
             res += i
         else:
-            res += chr((ord(i) % 26) + shift)
+            res += chr((ord(i)) + shift)
     return res
