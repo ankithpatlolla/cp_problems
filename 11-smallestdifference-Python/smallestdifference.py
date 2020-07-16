2534b06fc533ab447172052aa78bddf977cd5485
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-17 01:45:20 +0530

    Jul 17  01:45:20

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -1,9 +1,14 @@
-# Write the function smallestDifference(a) that takes a list of integers and returns 
-# the smallest absolute difference between any two 
+# Write the function smallestDifference(a) that takes a list of integers and returns
+# the smallest absolute difference between any two
 # integers in the list. If the list is empty, return -1. For example:
 #       assert(smallestDifference([19,2,83,6,27]) == 4)
 # The two closest numbers in that list are 2 and 6, and their difference is 4.
 
 def smallestdifference(a):
-	# Your code goes here
-	pass
+    # Your code goes here
+    min = float('-inf')
+    for i in range(len(a) - 1):
+        diff = abs(a[i] - a[i + 1])
+        if diff < min:
+            min = diff
+    return min
