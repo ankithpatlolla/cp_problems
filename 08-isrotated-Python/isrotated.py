04da8864db172a6e63a6e475664ee318ad93fcc2
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-16 23:42:24 +0530

    Jul 16  23:42:24

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -6,4 +6,5 @@
 
 def isrotated(str1, str2):
     # Your code goes here
-    return str2 == str1[-1] + str1[:-1]
+    for i in range(len(str1) - 1, 0, -1):
+        return str2 == str1[i:] + str1[:i]
