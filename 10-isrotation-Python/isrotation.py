91af906c00b6c8a234e732b8bdfcd550cbffc705
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-18 01:01:03 +0530

    Jul 18  01:01:01

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -5,6 +5,19 @@
 # 3412 is a rotation of 1234. Any number 
 # is a rotation of itself.
 
+
+def num_count (n):
+	count = 0
+	while n:
+		n = n // 10
+		count += 1
+	return count	
 def isrotation(x, y):
 	# Your code goes here
+	digits = num_count(x)
+	expo = 10 ** (digits - 1)
+	for i in range(digits):
+		rem = x // expo
+		rotation = ((x * 10) + rem) - (rem * expo)
+
 	pass
