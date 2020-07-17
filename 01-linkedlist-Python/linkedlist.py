ad1c3550dd17d4ae1f6c6dd74ba421d852c02168
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-17 13:03:22 +0530

    Jul 17  13:03:22

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -8,26 +8,34 @@ particular value.
 Then, use "Test Run" and "Submit" to run the test cases
 at the bottom."""
 
+
 class Element(object):
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
+
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
-        
+
     def append(self, new_element):
         # Your code goes here
-        pass
-            
+        if self.head is None:
+            self.head = new_element
+        else:
+            temp = self.head
+            while temp is not None:
+                temp = temp.next
+            temp = new_element
+
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
         pass
-    
+
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
@@ -35,8 +43,7 @@ class LinkedList(object):
         the 2nd and 3rd elements."""
         # Your code goes here
         pass
-    
-    
+
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
