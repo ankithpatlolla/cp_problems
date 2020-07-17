2c26110ca1e8f7b02bc1080f8e41b50a63e7af90
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-17 23:28:15 +0530

    Jul 17  23:28:15

--- 07-isfactorish-Python/isfactorish.py
+++ 07-isfactorish-Python/isfactorish.py
@@ -1,6 +1,6 @@
-# Write the function fun_isfactorish(n) that takes a value int n, 
-# and returns True if n is a (possibly-negative) integer with exactly 3 unique digits 
-# (so no two digits are the same), where each of the digits is a factor of the number 
+# Write the function fun_isfactorish(n) that takes a value int n,
+# and returns True if n is a (possibly-negative) integer with exactly 3 unique digits
+# (so no two digits are the same), where each of the digits is a factor of the number
 # n itself. In all other cases, the function returns False (without crashing).
 # For example:
 #  assert(fun_isfactorish(412) == True) # 4, 1, and 2 are all factors of 412
@@ -10,7 +10,17 @@
 #  assert(fun_isfactorish(420) == False) # 420 has a 0 (0 is not a factor)
 #  assert(fun_isfactorish(42) == False) # 42 has a leading 0 (only 2 unique digits)
 
+def is_uniqe(n):
+    l = []
+    while n:
+        if n % 10 in l:
+            return False
+        l.append(n % 10)
+        n = int(n / 10)
+    return True
+
 
 def fun_isfactorish(n):
-	return False
-
+    if n < 100 or n > 999:
+        return False
+    return False
