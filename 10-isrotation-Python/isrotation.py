0464e4a9651d863e61b609d02f6b2865874df0cd
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-18 01:29:11 +0530

    Jul 18  01:29:09

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -34,10 +34,10 @@ def isrotation(x, y):
     expo = 10 ** (digits - 1)
     for i in range(digits - 1):
         x = int((x % 10) * expo + (x / 10))
-        print(x, "in isRotate")
+        # print(x, "in isRotate")
         if x == y:
             return True
     return False
 
 
-print(isrotation(10010, 10100))
+# print(isrotation(10010, 10100))
