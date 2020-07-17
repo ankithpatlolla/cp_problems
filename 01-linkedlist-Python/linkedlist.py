03bb1013d9e51cc5b4d76d68789d6df969da10e9
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-17 19:20:29 +0530

    Jul 17  19:20:28

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -58,7 +58,7 @@ class LinkedList(object):
         """Delete the first node with a given value."""
         # Your code goes here
         temp = self.head
-        if self.head.next != None and self.head.value == value:
+        if self.head.next.next != None and self.head.value == value:
             self.head = self.head.next
         else:
             while temp.next.next != None:
