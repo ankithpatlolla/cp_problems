e637d2c30bb993476ac7aa14bd50c652509c1f63
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-17 20:33:13 +0530

    Jul 17  20:33:11

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -36,7 +36,7 @@ class LinkedList(object):
         # Your code goes here
         pos = 1
         temp = self.head
-        while pos <= position:
+        while pos <= position and temp.next != None:
             if pos == position:
                 return temp
             else:
