8d4ca6a5be6cddef87010116f291ea0b48ba99b1
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-17 19:15:33 +0530

    Jul 17  19:15:32

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -62,7 +62,7 @@ class LinkedList(object):
             self.head = self.head.next
         else:
             while temp.next != None:
-                if temp.next.value == value:
+                if temp != None and temp.next.value == value:
                     node = temp.next
                     temp.next = node.next
                     break
