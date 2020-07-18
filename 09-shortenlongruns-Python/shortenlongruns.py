528027ad34e59b8e801bd7c6757f73861e6f056a
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-19 03:20:34 +0530

    Jul 19  03:20:33

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -18,7 +18,7 @@ def shortenlongruns(L, k):
         if L[i] == L[i + 1]:
             print(L[i])
             if count >= k:
-                return L[:i] + L[i - 2:]
+                return L[:i - 2] + L[i - 2:]
             count += 1
         i += 1
 
