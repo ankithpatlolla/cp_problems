f5b32ddcba3449bde5a08e99f4f0fa1ab1f33b95
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-18 20:09:04 +0530

    Jul 18  20:09:04

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -10,11 +10,10 @@ def fun_kth_occurrences(s, n):
         if ((s.count(i), i)) not in l and i != " ":
             l.append((s.count(i), i))
     l.sort(key=lambda x: x[0], reverse=True)
-    print(l)
+    # print(l)
     try:
         return l[n-1][1]
     except:
         return l[0][1]
 
-
-print(fun_kth_occurrences("hsdibdouvbeiovbfeibvdsioubidsbvviefadbcildfab", 4))
+# print(fun_kth_occurrences("hsdibdouvbeiovbfeibvdsioubidsbvviefadbcildfab", 4))
