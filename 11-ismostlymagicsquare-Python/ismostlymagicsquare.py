dcc97ebc47c6dc256e68370ede37165b18e3bf39
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-20 00:52:11 +0530

    Jul 20  00:52:10

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -1,9 +1,9 @@
 # isMostlyMagicSquare(a) [15 pts]
-# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square 
+# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square
 # with N>0, and returns True if it is "mostly magic" and False otherwise, where a square is "mostly magic" if:
 # Each row, each column, and each of the 2 diagonals each sum to the same total.
-# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers 
-# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also 
+# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers
+# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also
 # a "mostly magic" square, including this sample magic square:
 # Read for more: https://en.wikipedia.org/wiki/Magic_square
 # Here is another mostly-magic square:
@@ -13,6 +13,30 @@
 #   [ 2, 1]]
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
+
 def ismostlymagicsquare(a):
-	# Your code goes here
-	pass
+    # Your code goes here
+    n = len(a)
+    d1 = 0
+    for i in range(n):
+        d1 += a[i][i]
+    d2 = 0
+    for i in range(n):
+        d2 += a[i][n-1]
+    if d1 != d2:
+        return False
+    for i in range(n):
+        rsum = 0
+        for j in range(len(a[0])):
+            rsum += a[i][j]
+        if rsum != d1:
+            return False
+
+    for i in range(n):
+        lsum = 0
+        for j in range(len(a[0])):
+            lsum += a[j][i]
+        if lsum != d1:
+            return False
+
+    return True
