96d33dfd9ca4403df547a0b2234c227f365a5fcc
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-20 17:21:08 +0530

    Jul 20  17:21:08

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -27,7 +27,7 @@ class BinaryTree(object):
         """Helper method - use this to create a 
         recursive search solution."""
         # Your code goes here
-        if start == None or find_val is not None:
+        if start == None or find_val is None:
             return False
         if start.value == find_val:
             return True
