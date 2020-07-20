9514ab3c5e91201cecd5ee365450d4476c881a70
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-21 00:43:17 +0530

    Jul 21  00:43:17

--- 05-canqueenattack-Python/canqueenattack.py
+++ 05-canqueenattack-Python/canqueenattack.py
@@ -10,6 +10,6 @@ def diff(x, y):
 
 def canqueenattack(qR, qC, oR, oC):
     # Your code goes here
-    if diff(qR, oR) == diff(qC, oC) or (diff(qR, oR) == 1 and qC == oC) or (diff(qC, oC) == 1 and qR == oR):
+    if diff(qR, oR) == diff(qC, oC) or qC == oC or qR == oR:
         return True
     return False
