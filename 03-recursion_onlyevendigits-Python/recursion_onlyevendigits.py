8be67e089e43e132fa657316239b11dc31a51f26
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-21 22:22:24 +0530

    Jul 21  22:22:24

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -1,12 +1,31 @@
-# Without using iteration and without using strings, 
-# write the recursive function onlyEvenDigits(L), 
-# that takes a list L of non-negative integers 
-# (you may assume that), and returns a new list of 
-# the same numbers only without their odd digits 
-# (if that leaves no digits, then replace the number with 0). 
-# So: onlyEvenDigits([43, 23265, 17, 58344]) returns [4, 226, 0, 844]. 
-# Also the function returns the empty list if the original list is empty. 
+# Without using iteration and without using strings,
+# write the recursive function onlyEvenDigits(L),
+# that takes a list L of non-negative integers
+# (you may assume that), and returns a new list of
+# the same numbers only without their odd digits
+# (if that leaves no digits, then replace the number with 0).
+# So: onlyEvenDigits([43, 23265, 17, 58344]) returns [4, 226, 0, 844].
+# Also the function returns the empty list if the original list is empty.
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
-def fun_recursion_onlyevendigits(l): 
-		return []
+def even(n):
+    if n == 0:
+        return 0
+    rem = n % 10
+    n = n // 10
+    if rem % 2 == 0:
+        return even(n) * 10 + rem
+    return even(n)
+
+
+def even_digits(l, res):
+    if l == [] or not l:
+        return res
+    num = l.pop(0)
+    res.append(even(num))
+    return even_digits(l, res)
+
+
+def fun_recursion_onlyevendigits(l):
+    res = []
+    return even_digits(l, res)
