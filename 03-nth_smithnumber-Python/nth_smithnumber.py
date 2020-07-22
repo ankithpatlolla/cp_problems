fde62ce69a747c5f99f1ecbc920abd3e7e125fcb
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-22 17:46:27 +0530

    Jul 22  17:46:26

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -25,11 +25,11 @@ def is_smith(n):
     l = []
     while n % 2 == 0:
         l.append(2)
-        n = n / 2
+        n = n // 2
     for i in range(3, n):
         while n % i == 0:
             l.append(i)
-            n = n / i
+            n = n // i
     if n > 2:
         l.append(n)
     c = total(l, 0)
