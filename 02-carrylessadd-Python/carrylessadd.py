89e1dad1229c01de1baff0f1613de37c51666a9e
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-22 12:26:58 +0530

    Jul 22  12:26:57

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -13,6 +13,7 @@ def fun_carrylessadd(x, y):
         r1, r2 = x % 10, y % 10
         if r1 + r2 < 10:
             res += str(r1 + r2)
-        res += str((r1 + r2) % 10)
+        if (r1 + r2) % 10 != 0:
+            res += str((r1 + r2) % 10)
         x, y = x // 10, y // 10
     return int(res)
