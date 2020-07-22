8d5a5bdfb7dc46df095af0832af99935902d12b5
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-22 17:48:23 +0530

    Jul 22  17:48:23

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -20,7 +20,24 @@ def total(l, s):
     return total(l, s)
 
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
 def is_smith(n):
+    if is_prime(n):
+        return False
     i = 2
     l = []
     while n % 2 == 0:
