7ef02c7f26fc231cc49b8b743428af149ff6e913
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-24 00:13:12 +0530

    Jul 24  00:13:12

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -6,21 +6,17 @@
 # note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again)....
 
 def is_prime(n):
-    # if n <= 1:
-    #     return False
-    # if n <= 3:
-    #     return True
-    # if n % 2 == 0 or n % 3 == 0:
-    #     return False
-    # i = 5
-    # while i * i <= n:
-    #     if n % i == 0 or n % (i + 2) == 0:
-    #         return False
-    #     i += 6
-    # return True
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
 
