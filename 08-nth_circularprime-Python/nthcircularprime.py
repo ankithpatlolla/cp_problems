7886736b5123e1c82c6dbf99258196fa1ad95a9b
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-23 23:25:10 +0530

    Jul 23  23:25:07

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -3,7 +3,7 @@
 # Circular prime, which is a prime number that does not contain any 0's and such that all the
 # numbers resulting from rotating its digits are also prime. The first Circular primes are 2, 3,
 # 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime,
-# note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again).
+# note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again)..
 
 def is_prime(n):
     if n <= 1:
