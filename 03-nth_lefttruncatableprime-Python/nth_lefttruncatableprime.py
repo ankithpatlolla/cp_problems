7ac00a6ebb45478fedde83cb12f968b0d92e6b0e
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-23 14:39:21 +0530

    Jul 23  14:39:21

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -10,9 +10,33 @@
 import math
 
 
-def fun_nth_lefttruncatableprime(n):
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+    return True
+
+
+def is_leftprime(n):
     while n > 0:
         rem = n % 10
         if not is_prime(rem):
             return False
+        n //= 10
     return True
+
+
+def fun_nth_lefttruncatableprime(n):
+    i = 1
+    while n > 0:
+        i += 1
+        if is_leftprime(i):
+            n -= 1
+    return i
