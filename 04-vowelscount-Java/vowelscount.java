90e52b7268bc9c0ce4806cdc067213ac24785dbb
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-07-25 20:36:43 +0530

    Jul 25  20:36:43

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -7,7 +7,14 @@
 class vowelscount {
 	public int fun_vowelscount(String s){
 		// your code goes here
-		return 0;
+		int c = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				c++;
+			}
+		}
+		return c;
 	}
 	
 }
